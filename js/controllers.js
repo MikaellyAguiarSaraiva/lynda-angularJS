6c73afb5b9c8e0c3b02be06bfafbbea7f65c05b4
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-10-29 14:42:17 -0200

    Wiring up a second partial

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,13 @@ var artistControllers = angular.module('artistControllers', []);
 artistControllers.controller('ListController', ['$scope', '$http', function ($scope, $http){
 	$http.get('js/data.json').success(function(data) {
 		$scope.artists = data;
-		$scope.artistOrder = '	name';
+		$scope.artistOrder = 'name';
+	});	
+}]);
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams){
+	$http.get('js/data.json').success(function(data) {
+		$scope.artists = data;
+		$scope.whichItem = $routeParams.itemId;
 	});	
 }]);
